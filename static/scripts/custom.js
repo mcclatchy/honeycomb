c4646cb744a3e13340c09d873a3e6dee207805cc
Kristi Walker <kristiwalker22@gmail.com>
2016-12-07 15:36:31 -0500

    finishes sass compilation

--- static/scripts/custom.js
+++ static/scripts/custom.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     $("#includedContent").load("../../build/custom.html");
     $('body').css('overflow','hidden');
     // makes container full-width
-    $(".head-img").closest("section").removeClass("container").addClass("container-fluid");
+    $(".full-width").closest("section").removeClass("container").addClass("container-fluid");
     // when everything is loaded, do these things
     $(window).load(function() {
-        $('.head-img').show();
+        $('.full-width').show();
         $("html, body").scrollTop(0);
         $(".loader").fadeOut("slow");
         $('body').css('overflow','auto');
