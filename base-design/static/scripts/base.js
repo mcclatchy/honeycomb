d2ed3e1435d27deff983368678ef946846e07cc4
Kristi Walker <kristiwalker22@gmail.com>
2016-12-09 15:06:49 -0500

    adds omse of the new image sizes

--- base-design/static/scripts/base.js
+++ base-design/static/scripts/base.js
@@ -4,22 +4,24 @@ $(document).ready(function() {
         $('.loader-logo').html(logo);
 
     // makes container and row full-width
-        var fullWidth = $(".full-width").closest("section");
-        fullWidth.removeClass("container").addClass("container-fluid");
-        fullWidth.find('.row').removeClass("row").addClass("row-fluid");
-        fullWidth.find(".col-sm-12").css('padding', '0');
-        // $(".full-width").closest(".col-sm-12").css('padding', '0');
+        var fluidImg = $('.fluid-img').closest('section');
+        fluidImg.removeClass('container').addClass('container-fluid');
+        fluidImg.find('.row').removeClass('row').addClass('row-fluid');
+        fluidImg.find('.col-sm-12').css('padding', '0');
+        $('.container-img').closest('section').find('.col-sm-12').css('padding', '0');
+
+        // $('.fluid-img').closest('.col-sm-12').css('padding', '0');
 
     // loader scripts while elements load/rewrite
         // removes scroll
         $('body').css('overflow','hidden');
         // shows full-width img after delay for rewriting
-        $('.full-width').delay(3000).show();
+        $('.fluid-img').delay(3000).show();
         // fades out loader content
-        $(".loader").delay(2000).fadeOut("slow");
+        $('.loader').delay(2000).fadeOut('slow');
         // scrolls to top during fade if user has scrolled
-        $(".loader-wrapper").delay(3000).fadeOut("slow");
-        $("html, body").scrollTop(0);
+        $('.loader-wrapper').delay(3000).fadeOut('slow');
+        $('html, body').scrollTop(0);
         // shows scroll bar after loader fades
         $('body').delay(4000).css('overflow','auto');
 });
