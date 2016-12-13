76786268a407e72bacbb8602b4b5de528ad7cf5e
Kristi Walker <kristiwalker22@gmail.com>
2016-12-13 15:11:25 -0500

    adds social urls minus twitter and new custom dependency structuring

--- base-design/static/scripts/base.js
+++ base-design/static/scripts/base.js
@@ -1,27 +1,41 @@
 $(document).ready(function() {
-    // adds logo to loader
+
+    addLoader();
+    setImgWidth();
+    setShareURL();
+
+    function addLoader() {
         var logo = $('#masthead-logo').html();
         $('.loader-logo').html(logo);
 
-    // makes container and row full-width
+        $('body').css('overflow', 'hidden');
+        $('.fluid-img').delay(3000).show();
+        $('.loader').delay(2000).fadeOut('slow');
+        $('.loader-wrapper').delay(3000).fadeOut('slow');
+        $('html, body').scrollTop(0);
+        $('body').delay(4000).css('overflow', 'auto');
+    }
+
+    function setImgWidth() {
         var fluidImg = $('.fluid-img').closest('section');
         fluidImg.removeClass('container').addClass('container-fluid');
         fluidImg.find('.row').removeClass('row').addClass('row-fluid');
         fluidImg.find('.col-sm-12').css('padding', '0');
         $('.container-img').closest('section').find('.col-sm-12').css('padding', '0');
+    }
 
-        // $('.fluid-img').closest('.col-sm-12').css('padding', '0');
+    function setShareURL() {
+        var projectURL = window.location.href;
 
-    // loader scripts while elements load/rewrite
-        // removes scroll
-        $('body').css('overflow','hidden');
-        // shows full-width img after delay for rewriting
-        $('.fluid-img').delay(3000).show();
-        // fades out loader content
-        $('.loader').delay(2000).fadeOut('slow');
-        // scrolls to top during fade if user has scrolled
-        $('.loader-wrapper').delay(3000).fadeOut('slow');
-        $('html, body').scrollTop(0);
-        // shows scroll bar after loader fades
-        $('body').delay(4000).css('overflow','auto');
+        var facebookURL = "http://www.facebook.com/sharer/sharer.php?u=" + projectURL;
+        $('#facebook-share').attr("href", facebookURL);
+
+        var twitterPartial = "https://twitter.com/intent/tweet?text=Voters%20Make%20the%20Call%3A%2025%20States%2069%20Sentiments%20";
+        var twitterURL = twitterPartial + projectURL;
+        $("#twitter-share").attr("href", twitterURL);
+
+        var emailPartial = "mailto:?subject=Voters Make the Call: 69 voicemails from 25 states&body=";
+        var emailURL = emailPartial + projectURL;
+        $("#email-share").attr("href", emailURL);
+    }
 });
