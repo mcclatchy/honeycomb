364f07d56e658a28b83596c70b40457a6d7675ab
Kristi Walker <kristiwalker22@gmail.com>
2016-12-19 16:40:30 -0500

    styles h3

--- base-design/static/scripts/base.js
+++ base-design/static/scripts/base.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+    console.log("hello");
+
+    $.getJSON('static/scripts/sites.json', function(data) {
+        addSiteColor(data);
+        addLoader();
+        setImgWidth();
+        setShareURL();
+    });
+
+    // look through all site urls and find the one that your page matches
+    // add the class that corresponds to that url
+    function addSiteColor(data) {
+        var siteURL = mast_options.publicationUrl;
+
+        $.each(data, function(k, v) {
+            if (v.site === siteURL) {
+                $('.primary-color').addClass(v.name + "-primary-color");
+                $('.primary-background').addClass(v.name + "-primary-background");
+                $('.accent-color').addClass(v.name + "-accent-color");
+                $('.accent-background').addClass(v.name + "-accent-background");
+                var accentColor = v.name + "-accent-color";
+            }
+        });
+    }
 
     function addLoader() {
         var logo = $('#masthead-logo').html();
@@ -24,55 +48,31 @@ $(document).ready(function() {
         var projectURL = window.location.href;
         projectURL = "http://www.mcclatchydc.com/news/nation-world/world/article117747598.html";
 
+        // Facebook
         var facebookURL = "http://www.facebook.com/sharer/sharer.php?u=" + projectURL;
         $('#facebook-share').attr("href", facebookURL);
 
-        /* Clean up ampersands, octothorpes, and pluses */
+        // Twitter
         var metaTitle = $('meta[name=title]').attr('content');
         var title = metaTitle.split(' | ');
-
         var twitterPartial = encodeURI(title[0]);
         twitterPartial = amperOctoPlus(twitterPartial);
         var twitterURL = "https://twitter.com/home?status=" + twitterPartial + "%20" + projectURL;
         $("#twitter-share").attr("href", twitterURL);
 
+        // Email
         var emailPartial = "mailto:?subject=Voters Make the Call: 69 voicemails from 25 states&body=";
         var emailURL = emailPartial + projectURL;
         $("#email-share").attr("href", emailURL);
     }
 
-    function amperOctoPlus(s) {
-    	s = s.replace(/&/g, '%26');
-    	s = s.replace(/#/g, '%23');
-    	s = s.replace(/\+/g, '%2B');
-    	s = s.replace(/@/g, '%40');
-    	s = s.replace(/:/g, '%3A');
-    	return s;
+    // Clean up ampersands, octothorpes, and pluses
+    function amperOctoPlus(url) {
+        url = url.replace(/&/g, '%26');
+        url = url.replace(/#/g, '%23');
+        url = url.replace(/\+/g, '%2B');
+        url = url.replace(/@/g, '%40');
+        url = url.replace(/:/g, '%3A');
+        return url;
     }
-
-    $.getJSON('static/scripts/colors.json', function(data) {
-        addSiteColor(data);
-        addLoader();
-        setImgWidth();
-        setShareURL();
-    });
-
-    // look through all site urls and find the one that your page matches
-    // add the class that corresponds to that url
-    function addSiteColor(data) {
-        var siteURL = mast_options.publicationUrl;
-
-        $.each(data, function(k, v) {
-            if (v.site === siteURL) {
-                $('.primary-color').addClass(v.name + "-primary-color");
-                $('.primary-background').addClass(v.name + "-primary-background");
-                $('.primary-color').addClass(v.name + "-primary-color");
-                $('.primary-background').addClass(v.name + "-primary-background");
-            }
-        });
-    }
-
-    // if (siteURL === 'http://www.islandpacket.com/') {
-    //     $('.primary-background').addClass('beaufortgazette-primary-background');
-    // }
 });
