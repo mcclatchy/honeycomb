8bfc8b836c783586e0d5cdda2f0bb417fdc63af0
Kristi Walker <kristiwalker22@gmail.com>
2017-03-28 18:02:16 -0400

    adds gulp scaffolding + handlebars mess

---
+++ build/static/scripts/base.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+    var storyTemplate = Handlebars.templates.story;
+    $("#story").append(storyTemplate);
+
+    /********************************************************
+    ADD LINKS TO SHARE ICONS
+    ********************************************************/
+
+    var projectURL = window.location.href;
+    projectURL = "http://www.mcclatchydc.com/news/nation-world/national/article138656403.html";
+
+    // Facebook
+    var facebookURL = "http://www.facebook.com/sharer/sharer.php?u=" + projectURL;
+    $('#hc-facebook-share').attr("href", facebookURL);
+
+    // Twitter
+    var metaTitle = $('meta[name=title]').attr('content');
+    var title = metaTitle.split(' | ');
+    var twitterPartial = encodeURI(title[0]);
+    twitterPartial = amperOctoPlus(twitterPartial);
+    var twitterURL = "https://twitter.com/home?status=" + twitterPartial + "%20" + projectURL;
+    $("#hc-twitter-share").attr("href", twitterURL);
+
+    // Email
+    var emailPartial = "mailto:?subject=Paul Ryan’s<br /> Obamacare replacement bill is in serious trouble&body=";
+    var emailURL = emailPartial + projectURL;
+    $("#hc-email-share").attr("href", emailURL);
+
+    // Clean up ampersands, octothorpes, and pluses
+    function amperOctoPlus(url) {
+        url = url.replace(/&/g, '%26');
+        url = url.replace(/#/g, '%23');
+        url = url.replace(/\+/g, '%2B');
+        url = url.replace(/@/g, '%40');
+        url = url.replace(/:/g, '%3A');
+        return url;
+    }
+
+    /********************************************************
+    GET AND ADD NEWSROOM COLORS
+    ********************************************************/
+
+    $.getJSON('static/scripts/newsroom-sites.json', function(data) {
+        addSiteColor(data);
+    });
+
+    /*
+    Look through all site urls and find the one that your page matches.
+    Add the class that corresponds to that url.
+    */
+
+    function addSiteColor(data) {
+        var siteURL = mast_options.publicationUrl;
+
+        $.each(data, function(k, v) {
+            if (v.site === siteURL) {
+                $('.hc-primary-color').addClass(v.name + "-primary-color");
+                $('.hc-primary-background').addClass(v.name + "-primary-background");
+                $('.hc-accent-color').addClass(v.name + "-accent-color");
+                $('.hc-accent-background').addClass(v.name + "-accent-background");
+            }
+        });
+    }
+});
---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+// minifies js
+var uglify = require('gulp-uglify');
+// checks if certain file type
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var concat = require('gulp-concat');
+
+/*******************************************************************************
+DEVELOPMENT TASKS
+*******************************************************************************/
+
+// runs all development tasks in order listed with the 'gulp' command (default)
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'browserSync', 'watch'],
+        callback
+    );
+});
+
+// reloads browser on file save
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'build'
+        },
+    });
+});
+
+// compiles sass to css
+gulp.task('sass', function() {
+    // gets all files ending with .scss in static/sass
+    return gulp.src('build/static/sass/**/*.scss')
+        // .pipe(changed('prod'))
+        .pipe(sass())
+        .pipe(gulp.dest('build/static/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('build/static/sass/**/*.scss', ['sass']);
+    gulp.watch('build/*.html', browserSync.reload);
+    gulp.watch('build/static/scripts/**/*.js', browserSync.reload);
+});
+
+gulp.task('templates', function(){
+  gulp.src('build/templates/*.hbs')
+    .pipe(handlebars())
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'story.templates',
+      noRedeclare: true, // Avoid duplicate declarations
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('build/static/scripts/'));
+});
+
+/*******************************************************************************
+PRODUCTION TASKS
+*******************************************************************************/
+
+// sequences build task to run after production tasks
+gulp.task('prod', function(callback) {
+    // runs each task in order listed
+    runSequence('clean:prod', ['sass', 'useref',  'images', 'fonts'],
+        callback
+    );
+});
+
+// combines simliar file to one file in prod/static folder
+gulp.task('useref', function() {
+    return gulp.src('build/*.html')
+        .pipe(useref())
+        // minifies only if it's a JS file
+        .pipe(gulpIf('*.js', uglify()))
+        // minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('prod'));
+});
+
+// optimiazes images
+gulp.task('images', function() {
+    return gulp.src('build/static/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        // caches images that ran through imagemin
+        .pipe(cache(imagemin([
+                imagemin.gifsicle({
+                    interlaced: true
+                }),
+                imagemin.jpegtran({
+                    progressive: true
+                }),
+                imagemin.optipng({
+                    optimizationLevel: 5
+                }),
+                imagemin.svgo({
+                    plugins: [{
+                        removeViewBox: true
+                    }]
+                }), {
+                    verbose: true
+                }
+            ]))
+            .pipe(gulp.dest('prod/static/images')));
+});
+
+// copies fonts to prod/static/fonts
+gulp.task('fonts', function() {
+    return gulp.src('build/static/fonts/**/*')
+        .pipe(gulp.dest('prod/static/fonts'));
+});
+
+// deletes unused files in prod
+gulp.task('clean:prod', function() {
+    return del.sync('prod');
+});
+
+/*
+ * clears cache (usually means images)
+ * must be manually run (not set up with build task)
+ */
+gulp.task('cache:clear', function(callback) {
+    return cache.clearAll(callback);
+});
